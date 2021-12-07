88e10b35eec2da3152b2d5ac559178b18bc62a61
liutz <liutangzhao@maoyan.com>
2021-12-07 19:46:12 +0800

    userDAO

--- user/user-service/src/main/java/com/miex/user/domain/dao/UserDAO.java
+++ user/user-service/src/main/java/com/miex/user/domain/dao/UserDAO.java
@@ -2,18 +2,7 @@ package com.miex.user.domain.dao;
 
 import com.miex.domain.dao.ElasticsearchDAO;
 import com.miex.user.domain.entity.User;
-import com.miex.util.StringUtil;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.GetIndexRequest;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 
 /**
  * @author liutz
